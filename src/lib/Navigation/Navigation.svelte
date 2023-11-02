41dca28d41672f5e8a0d5d48ad0d2cdd7567b44b
scpmx <13601394+scpmx@users.noreply.github.com>
2023-11-02 17:05:50 -0400

    Add navigation component

---
+++ src/lib/Navigation/Navigation.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+    import { getDrawerStore } from "@skeletonlabs/skeleton";
+
+    const drawerStore = getDrawerStore();
+
+    function drawerClose(): void {
+        drawerStore.close();
+    }
+
+    let pages = [
+        { name: "Home", path: "/"},
+        { name: "Results Summary", path: "/results-summary"}
+    ]
+</script>
+
+<nav class="list-nav p-4">
+    <ul>
+        {#each pages as page}
+            <li><a href="{page.path}" on:click={drawerClose}>{page.name}</a></li>
+        {/each}
+    </ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,52 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+    import Navigation from '$lib/Navigation/Navigation.svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	initializeStores();
+	const drawerStore = getDrawerStore();
+
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
 </script>
 
+
+<Drawer>
+	<Navigation/>
+</Drawer>
+
 <!-- App Shell -->
-<AppShell>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
+
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation/>
+	</svelte:fragment>
+
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<strong class="text-xl uppercase">Skeleton</strong>
+				</div>
 			</svelte:fragment>
+
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
