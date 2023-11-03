fa0deaa508981b4839a815cc043f8aab92fb9453
scpmx <13601394+scpmx@users.noreply.github.com>
2023-11-02 22:00:34 -0400

    Add initial results summary page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,35 +43,8 @@
 							</svg>
 						</span>
 					</button>
-					<strong class="text-xl uppercase">Skeleton</strong>
+					<strong class="text-xl uppercase">My UI Portfolio</strong>
 				</div>
-			</svelte:fragment>
-
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
--- src/routes/results-summary/+page.svelte
+++ src/routes/results-summary/+page.svelte
@@ -1,3 +1,72 @@
-<script lang="ts">
+<div class="h-full bg-slate-100">
+    <div class="container mx-auto my-auto">
+        <div class="flex flex-col items-center justify-center bg-blue-600 rounded-b-3xl">
+            <span class="text-slate-300 text-xl pt-4 pb-4">
+                Your Result
+            </span>
+            <div class="flex flex-col items-center justify-center rounded-full bg-blue-700 w-32 h-32">
+                <span class="text-5xl font-bold">
+                    76
+                </span>
+                <span class="text-xs">
+                    of 100
+                </span>
+            </div>
+            <span class="text-center text-xl font-semibold m-2">
+                Great
+            </span>
+            <span class="text-center text-sm w-60 m-2">
+                You scored higher than 65% of the people who have taken these tests.
+            </span>
+        </div>
+        <div class="p-4 flex flex-col space-y-6">
+            <span class=" text-slate-700 font-bold">
+                Summary
+            </span>
 
-</script>
+            <div class="flex bg-red-100 rounded-lg p-4">
+                <img src="/results-summary/icon-reaction.svg" alt='Reaction' class="h-6 w-6 rounded-full mr-2" />
+                <span class="text-red-400 text-xl">
+                    Reaction
+                </span>
+                <span class="ml-auto items-end text-slate-600 text-xl">
+                  80 / 100
+                </span>
+              </div>
+              
+            <div class="flex items-start bg-yellow-100 rounded-lg p-4">
+                <img src="/results-summary/icon-memory.svg" alt='Reaction' class="h-6 w-6 rounded-full mr-2" />
+                <span class="text-yellow-400 text-xl">
+                    Memory
+                </span>
+                <span class="ml-auto items-end text-slate-600 text-xl">
+                    80 / 100
+                </span>
+            </div>
+
+            <div class="flex items-start bg-green-100 rounded-lg p-4">
+                <img src="/results-summary/icon-verbal.svg" alt='Reaction' class="h-6 w-6 rounded-full mr-2" />
+                <span class="text-green-400 text-xl">
+                    Verbal
+                </span>
+                <span class="ml-auto items-end text-slate-600 text-xl">
+                    80 / 100
+                </span>
+            </div>
+
+            <div class="flex items-start bg-blue-100 rounded-lg p-4">
+                <img src="/results-summary/icon-visual.svg" alt='Reaction' class="h-6 w-6 rounded-full mr-2" />
+                <span class="text-blue-400 text-xl">
+                    Visual
+                </span>
+                <span class="ml-auto items-end text-slate-600 text-xl">
+                    80 / 100
+                </span>
+            </div>
+
+            <button class="btn bg-slate-800">
+                Continue
+            </button>
+        </div>
+    </div>
+</div>
